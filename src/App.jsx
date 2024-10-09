94f07c8644808082a4137a0d5217e0022bd5aa02
Jamil Aktar <Jamilaktar227@gmail.com>
2024-10-09 08:45:37 +0530

    Add Frontedn Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import AuthService from './appwrite/auth.js'
+import { login, logout } from "./store/authslice.js"
+import { Footer, Header } from './compenents/index.js'
+import { Outlet } from 'react-router-dom'
 
 function App() {
- console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+  const [loading, setLoading ] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>A blog with appwrite</h1>
-    </>
-  )
-} 
+  useEffect(() => {
+    AuthService.getCurrentUser()
+    .then((userData) => {
+      if (userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400 '>
+    <div className="w-full block">
+      <Header />
+      <main>
+        TODO{/* <Outlet /> */}
+      </main>
+      <Footer />
+    </div>
+    </div>
+  ) : null
+}
 
 export default App
---
+++ src/compenents/index.js
@@ -0,0 +1,8 @@
+import Header from './Header/Header.jsx';
+import Footer from './Footer/Footer.jsx';
+
+export default {
+    Header,
+    Footer
+    // Add other components here as needed
+};
