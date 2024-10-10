936c1904783affc7234ea2fe9d9dbf9089d3f268
Jamil Aktar <Jamilaktar227@gmail.com>
2024-10-10 14:51:15 +0530

    somef files

---
+++ src/compenents/Siginup.jsx
@@ -0,0 +1,91 @@
+import React, {useState} from 'react'
+import authService from '../appwrite/auth'
+import { Link, useNavigate } from 'react-router-dom'
+import { login } from '../store/authSlice'
+import {Button, Input, Logo} from './index'
+import { useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
+
+function Siginup() {
+    const navigate = useNavigate()
+    const [error, setError] = useState("")
+    const dispatch = useDispatch()
+    const { register, handleSubmit } = useForm()
+
+    const create = async(data) => {
+        setError("")
+        try {
+            const userdata = await authService.createAccount(data)
+            if(userdata){
+                const userData = await authService.getCurrentUser()
+                if(userData) dispatch(login(userdata));
+                navigate("/")
+            }
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+  return (
+    <div className="flex items-center justify-center">
+            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}> <div className="mb-2 flex justify-center">
+                    <span className="inline-block w-full max-w-[100px]">
+                        <Logo width="100%" />
+                    </span>
+                </div>
+                <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
+                <p className="mt-2 text-center text-base text-black/60">
+                    Already have an account?&nbsp;
+                    <Link
+                        to="/login"
+                        className="font-medium text-primary transition-all duration-200 hover:underline"
+                    >
+                        Sign In
+                    </Link>
+                </p>
+                {error && <p className="text-red-600 my-8 text-center">{error}</p>}
+                <form onSubmit={handleSubmit(create)}>
+                    <div className="space-y-5">
+                        <Input 
+                        label="Full Name: "
+                        placeholder="Enter your full name"
+                        {...register("name", {
+                            required: true,
+                            
+                        })}
+                        />
+                        <Input
+                        label="Email"
+                        placeholder="Enter your email"
+                        type="email"
+                        {...register("email", {
+                            required: true,
+                            pattern: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(value) ||
+                            "Email adress must be a valid address"
+                        })}
+                        />
+                        <Input
+                        label="password"
+                        placeholder="Enter your password"
+                        type="password"
+                        {...register("password", {
+                            required: true,
+                            minLength: 8,
+                            pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value) ||
+                            "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+                        })}
+                        />
+                        <Button
+                            type="submit"
+                            className="w-full btn-success"
+                        >
+                            Create Account
+                        </Button>
+                    </div>
+                </form>
+            </div>
+    </div>
+
+  )
+}
+
+export default Siginup
